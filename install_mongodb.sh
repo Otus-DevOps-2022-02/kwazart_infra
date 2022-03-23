4134747ae5216a5e8c0c878f10bdd31f0811eedf
artempolozov <polartpav@yandex.ru>
2022-03-23 18:30:32 +0300

    change mongo script

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-sudo bash -c 'echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list'
-apt-get --assume-yes update
+curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+
+
+apt-get --assume-yes --allow-insecure-repositories update
+apt-get install --assume-yes --reinstall gpgv libgcrypt20
 apt-get --assume-yes install mongodb-org
-exec bash
 systemctl start mongod
 systemctl enable mongod
+systemctl status mongod
