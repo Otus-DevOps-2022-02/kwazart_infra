a47046466407cd256e50e0267540644a857294db
artempolozov <polartpav@yandex.ru>
2022-03-23 19:36:08 +0300

    change mongo script

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-sudo systemctl start mongodb
-sudo systemctl status mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list << EOF
+deb https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse
+EOF
+
+curl https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+
+sudo apt update
+
+# WireGuard server support
+sudo apt -y install wireguard wireguard-tools
+
+sudo ufw disable
+
+sudo apt -y install mongodb-org
+sudo systemctl enable mongod pritunl
+sudo systemctl start mongod pritunl
