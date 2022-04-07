a9009d53ca2727e749eb0e874f25b1189929c97b
artempolozov <polartpav@yandex.ru>
2022-04-07 22:39:26 +0300

    terraform (2 instance + load balancer)

---
+++ terraform/variables.tf
@@ -0,0 +1,36 @@
+variable "cloud_id" {
+  description = "Cloud"
+}
+variable "image_id" {
+  description = "Image"
+}
+variable "folder_id" {
+  description = "Folder"
+}
+variable "token" {
+  description = "Yandex token"
+}
+variable "zone" {
+  description = "Zone"
+  # Значение по умолчанию
+  default = "ru-central1-a"
+}
+variable "public_key_path" {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+variable "private_key_path" {
+  # Описание переменной
+  description = "Path to the private key used for ssh access"
+}
+variable "subnet_id" {
+  description = "Subnet"
+}
+variable "region_id" {
+  description = "Region id"
+  default     = "ru-central1"
+}
+variable "count_instance" {
+  description = "Count of instances"
+  default     = 2
+}
