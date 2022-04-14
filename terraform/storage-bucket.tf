f34b771bb4cebde85d85096add57c5ed7bd5ab16
artempolozov <polartpav@yandex.ru>
2022-04-14 23:46:27 +0300

    added storage-bucket, replaced vpc

---
+++ terraform/storage-bucket.tf
@@ -0,0 +1,13 @@
+// Use keys to create bucket
+resource "yandex_storage_bucket" "test" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket     = "tf-test-bucket"
+}
+
+provider "yandex" {
+  token     = var.token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.zone
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,46 @@
+variable "cloud_id" {
+  description = "Cloud"
+}
+variable "image_id" {
+  description = "Image"
+}
+variable "folder_id" {
+  description = "Folder"
+}
+variable "token" {
+  description = "Yandex token"
+}
+variable "zone" {
+  description = "Zone"
+  # Значение по умолчанию
+  default = "ru-central1-a"
+}
+variable "public_key_path" {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+variable "private_key_path" {
+  # Описание переменной
+  description = "Path to the private key used for ssh access"
+}
+variable "subnet_id" {
+  description = "Subnet"
+}
+variable "region_id" {
+  description = "Region id"
+  default     = "ru-central1"
+}
+variable "count_instance" {
+  description = "Count of instances"
+  default     = 1
+}
+
+variable "app_disk_image" {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+
+variable "db_disk_image" {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
+}
