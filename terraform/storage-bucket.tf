e5d6741133e5df53b96e4977514bf032bf22b669
artempolozov <polartpav@yandex.ru>
2022-04-14 23:52:28 +0300

    removed comment simbols from storage-bucket.tf

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,4 +1,3 @@
-// Use keys to create bucket
 resource "yandex_storage_bucket" "test" {
   access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
   secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
